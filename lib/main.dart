5f7fd2c1408678edf8bae4fe3ad78b3ea7409005
Payet Emmanuel <payet.emmanuel7777@gmail.com>
2022-02-18 09:44:34 +0400

    Random et Statefull

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -11,14 +10,14 @@ void main() {
           title: Center(child: Text('Ask Me Anything')),
           backgroundColor: Colors.lightBlue,
         ),
-        body: PoolPage(),
+        body: PoolPage2(),
       ),
     ),
   );
 }
 
 class PoolPage extends StatelessWidget {
-  int PoolNumber = 5;
+  int PoolNumber = Random().nextInt(5) + 1;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -29,6 +28,39 @@ class PoolPage extends StatelessWidget {
               padding: const EdgeInsets.all(8.0),
               child: TextButton(
                   onPressed: () {
+                    print("Vous avez appuyez sur la boule");
+                  },
+                  child: Image.asset("images/ball$PoolNumber.png")),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PoolPage2 extends StatefulWidget {
+  const PoolPage2({Key? key}) : super(key: key);
+
+  @override
+  _PoolPage2State createState() => _PoolPage2State();
+}
+
+class _PoolPage2State extends State<PoolPage2> {
+  int PoolNumber = Random().nextInt(5) + 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      PoolNumber = Random().nextInt(5) + 1;
+                    });
                     print("Vous avez appuyez sur la boule");
                   },
                   child: Image.asset("images/ball$PoolNumber.png")),
